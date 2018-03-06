39ac96e9d5480409ff70506550f7fb541f9ecdad
MarsiBarsi <darragon-nn@yandex.ru>
2018-03-06 08:48:58 +0300

    db tests

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import telebot
-from telebot import types
+import config
+import dbworker
+
 
 # bot init:
-TOKEN = ''
-
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(config.token)
 
 
+@bot.message_handler(commands=["start"])
+def cmd_start(message):
+    bot.send_message(message.chat.id, "Привет! Как я могу к тебе обращаться?")
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
+
+
+# По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message):
+    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Как тебя зовут?")
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
+
+
+'''
 # command /help
 @bot.message_handler(commands=['help'])
 def hlp(message):
@@ -32,10 +46,8 @@ def inline_catcher(call):
 
 def sleep_settings(message):
     sent = bot.send_message(message.chat.id, 'Во сколько ты хочешь вставать?')
-
     hours = bot.send_message(message.chat.id, 'Сколько часов в день ты готов спать?')
-    bot.send_message(message.chat.id,
-                     'хорошо, ты будешь вставать в ' + sent.text + ' и спать по ' + hours.text + ' часов')
-
+    bot.send_message(message.chat.id, 'хорошо, ты будешь вставать в ' + sent.text + ' и спать по ' + hours.text + ' часов')
+'''
 
 bot.polling(none_stop=True)
