3ee932d1d4adc7d70f38fb1d5a20a84b9cc824f3
Roman Sedov <darragon-nn@yandex.ru>
2018-03-08 07:13:23 +0300

    main upd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import config
 import dbworker
 
@@ -48,9 +49,9 @@ def user_entering_time(message):
         bot.send_message(message.chat.id, "Это неправильное время")
         return
     else:
-        # Возраст введён корректно, можно идти дальше
+        # Время введено корректно, можно идти дальше
         bot.send_message(message.chat.id, "Принято")
-        dbworker.set_state(message.chat.id, config.States.S_SEND_HOURS.value)
+        dbworker.set_state(message.chat.id, config.States.S_ENTER_HOURS.value)
 
 
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_HOURS.value)
@@ -66,36 +67,27 @@ def user_entering_hours(message):
         return
     else:
         # Возраст введён корректно, можно идти дальше
-        bot.send_message(message.chat.id, "окей")
-
-'''
-# command /help
-@bot.message_handler(commands=['help'])
-def hlp(message):
-    bot.send_message(message.chat.id, 'Привет, это помощь!')
-
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    key = types.InlineKeyboardMarkup()
-    but_1 = types.InlineKeyboardButton(text="Настройки твоего сна", callback_data="sleep_settings")
-    but_2 = types.InlineKeyboardButton(text="Как это работает?", callback_data="how_is_it_work")
-    key.add(but_1, but_2)
-    bot.send_message(message.chat.id, "Привет, я умный бот для сна. Выбери действие:", reply_markup=key)
+        bot.send_message(message.chat.id, "Теперь я буду говорить тебе, когда ложиться спать")
+        key = types.InlineKeyboardMarkup()
+        but_1 = types.InlineKeyboardButton(text="Настройки твоего сна", callback_data="sleep_settings")
+        but_2 = types.InlineKeyboardButton(text="Как это работает?", callback_data="how_is_it_work")
+        key.add(but_1, but_2)
+        bot.send_message(message.chat.id, "Если захочешь что-то изменить, нажимай на кнопки", reply_markup=key)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def inline_catcher(call):
     if call.data == 'sleep_settings':
-        sleep_settings(call.message)
+        dbworker.set_state(call.message.chat.id, config.States.S_ENTER_TIME.value)
+        bot.send_message(call.message, 'Введите новое время пробуждения: ')
     if call.data == 'how_is_it_work':
         hlp(call.message)
 
 
-def sleep_settings(message):
-    sent = bot.send_message(message.chat.id, 'Во сколько ты хочешь вставать?')
-    hours = bot.send_message(message.chat.id, 'Сколько часов в день ты готов спать?')
-    bot.send_message(message.chat.id, 'хорошо, ты будешь вставать в ' + sent.text + ' и спать по ' + hours.text + ' часов')
-'''
+# command /help
+@bot.message_handler(commands=['help'])
+def hlp(message):
+    bot.send_message(message.chat.id, 'Привет, это помощь!')
+
 
 bot.polling(none_stop=True)
